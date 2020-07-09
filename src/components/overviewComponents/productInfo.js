722122d0809771c9806adfdd8f5e1ad069010b5f
James Prytherch <jamesp802@hotmail.com>
2020-07-09 10:27:19 -0400

    deleted example data scrum

--- src/components/overviewComponents/productInfo.js
+++ src/components/overviewComponents/productInfo.js
@@ -193,51 +193,12 @@ const mapStateToProps = (state) => {
     rating: state.reviews
   };
 };
-
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchProductInfo: (id) => dispatch(fetchProductInfoAction(id)),
     fetchProductStyleList: (id) => dispatch(fetchProductStyleListAction(id)),
   };
 };
-// export default connect(null, null)(ProductInfo);
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(ProductInfo)
 );
-
-// const EXAMPLEDATA = {
-//   id: 123,
-//   name: "Gabe Slacks",
-//   slogan: "Sint est omnis recusandae rerum sunt qui.",
-//   description:
-//     "Nobis dolore dolor earum velit animi. Inventore voluptatem aspernatur maxime aperiam. Dolor voluptas voluptates animi et. Id saepe eveniet voluptatibus minus incidunt.",
-//   category: "Slacks",
-//   default_price: "868",
-//   features: [{ feature: "Sustainably Sourced", value: "null" }],
-// };
-
-// const styleData = {
-//   product_id: "123",
-//   results: [
-//     {
-//       style_id: 536,
-//       name: "Pink",
-//       original_price: "868",
-//       sale_price: "0",
-//       "default?": 1,
-//       photos: [
-//         {
-//           thumbnail_url:
-//             "https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-//           url:
-//             "https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
-//         },
-//         {
-//
-//           url:
-//             "https://images.unsplash.com/photo-1526948128573-703ee1aeb6fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-//         },
-//       ],
-//       skus: { XS: 2, S: 0, M: 27, L: 41, XL: 16, XXL: 35 },
-//     },
-// };
